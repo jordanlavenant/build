5be6bba23738b50cd24073bb452e5358a1bd27dc
jordan <jordan.lavenant@etu.univ-orleans.fr>
2024-12-11 14:08:50 +0100

    init

---
+++ CMakeFiles/td.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/savoyarde/TD5-multimedia/GLError.cpp" "CMakeFiles/td.dir/GLError.cpp.o" "gcc" "CMakeFiles/td.dir/GLError.cpp.o.d"
+  "/home/savoyarde/TD5-multimedia/repere.cpp" "CMakeFiles/td.dir/repere.cpp.o" "gcc" "CMakeFiles/td.dir/repere.cpp.o.d"
+  "/home/savoyarde/TD5-multimedia/td5.cpp" "CMakeFiles/td.dir/td5.cpp.o" "gcc" "CMakeFiles/td.dir/td5.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/savoyarde/TD5-multimedia
+# Build directory: /home/savoyarde/TD5-multimedia/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[SimpleTest]=] "td")
+set_tests_properties([=[SimpleTest]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/savoyarde/TD5-multimedia/CMakeLists.txt;71;add_test;/home/savoyarde/TD5-multimedia/CMakeLists.txt;0;")
